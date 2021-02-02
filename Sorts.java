0638cf66a43683abdef386af3eaf46b6aca34e25
cnelson20 <72891927+cnelson20@users.noreply.github.com>
2021-02-01 21:17:44 -0500

    Add files via upload

--- Sorts.java
+++ Sorts.java
@@ -15,4 +15,31 @@ public class Sorts {
       }
     } while (sorts > 0);
   }
+
+  public static void selectionSort(int[] data) {
+    int temp;
+    for (int i = 0; i < data.length- 1; i++) {
+      int min_key = i;
+      for (int j = i + 1; j < data.length; j++) {
+        if (data[j] < data[min_key]) {
+          min_key = j;
+        }
+      }
+      temp = data[min_key];
+      data[min_key] = data[i];
+      data[i] = temp;
+    }
+  }
+
+  public static void insertionSort(int[] data) {
+    int temp;
+    for (int i = 1; i < data.length; i++) {
+      temp = data[i];
+      int j;
+      for (j = i - 1; j >= 0 && temp < data[j]; j--) {
+        data[j+1] = data[j];
+      }
+      data[j+1] = temp;
+    }
+  }
 }
